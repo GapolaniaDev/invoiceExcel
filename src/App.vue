23cd73d5c0569b1a6d749f943970d75b9ddfb985
elpolakopolania <gustav0796@hotmail.com>
2024-05-21 19:15:51 +0930

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <div>
-    <Header />
-    <MainContent />
-    <Footer />
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import MainContent from './components/MainContent.vue'
-import Footer from './components/Footer.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    Header,
-    MainContent,
-    Footer
+    HelloWorld
   }
 }
 </script>
 
 <style>
-@import './assets/styles.css';
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/NOMBRE-DEL-REPO/'
-      : '/'
-  }
-  
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
