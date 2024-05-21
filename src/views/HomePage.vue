9b4c2aad4273e129d26664256497d07ac1e137d3
elpolakopolania <gustav0796@hotmail.com>
2024-05-21 23:15:38 +0930

    build

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,24 +1,39 @@
-<!-- src/views/HomePage.vue -->
 <template>
-  <ion-page>
+  <ion-menu content-id="main-content">
     <ion-header>
       <ion-toolbar>
-        <ion-title>Home</ion-title>
+        <ion-title>Menu Content</ion-title>
       </ion-toolbar>
     </ion-header>
-
-    <ion-content>
-      <h1>Home Page contenido de la pagina</h1>
-      <p>PPPPPPPP</p>
-    </ion-content>
+    <ion-content class="ion-padding">This is the menu content.</ion-content>
+  </ion-menu>
+  <ion-page id="main-content">
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button></ion-menu-button>
+        </ion-buttons>
+        <ion-title>Menu</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding"> Tap the button in the toolbar to open the menu. </ion-content>
   </ion-page>
 </template>
 
-<script>
-export default {
-  name: "HomePage",
-};
-</script>
+<script lang="ts">
+  import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+  import { defineComponent } from 'vue';
 
-<style scoped>
-</style>
+  export default defineComponent({
+    components: {
+      IonButtons,
+      IonContent,
+      IonHeader,
+      IonMenu,
+      IonMenuButton,
+      IonPage,
+      IonTitle,
+      IonToolbar,
+    },
+  });
+</script>
