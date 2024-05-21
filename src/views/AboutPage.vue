5a04e1c3442ad659082bd551e2b206bcd826d73c
elpolakopolania <gustav0796@hotmail.com>
2024-05-21 22:39:45 +0930

    init ionic

---
+++ src/views/AboutPage.vue
@@ -0,0 +1,23 @@
+<!-- src/views/AboutPage.vue -->
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>About title</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <h1>About Page</h1>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage'
+};
+</script>
+
+<style scoped>
+/* Agrega tus estilos aquí */
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,24 @@
+<!-- src/views/HomePage.vue -->
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <h1>Home Page contenido de la pagina</h1>
+      <p>PPPPPPPP</p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style scoped>
+</style>
