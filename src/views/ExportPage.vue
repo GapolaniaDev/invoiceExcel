774bd2224cc106bfc560494b807d014df0df254b
elpolakopolania <gustav0796@hotmail.com>
2024-06-26 13:54:16 +0930

    Clean Code Export Pdf

--- src/views/ExportPage.vue
+++ src/views/ExportPage.vue
@@ -23,7 +23,6 @@
     </ion-content>
   </ion-page>
 </template>
-
 <script lang="ts" setup>
 import {
   IonHeader,
@@ -33,251 +32,181 @@ import {
   IonPage,
   IonButton,
 } from "@ionic/vue";
-
 import { saveAs } from "file-saver";
 import ExcelJS from "exceljs";
 import { useStore } from "vuex";
+import { EXCEL_CONFIG, BORDER_STYLES } from "../models/invioceExcel"; // Suponiendo que has creado este archivo
 
 const store = useStore();
-const mainExcel = store.state.mainExcel;
-const employee = store.state.employee;
-const company = store.state.company;
+const { mainExcel, employee, company } = store.state;
 
-const TABLE_HEADER_START_NUMBER = 12;
-const TABLE_HEADER_COLUMNS_START_NUMBER = 13;
-const TABLE_BODY_START_NUMBER = 14;
 const TABLE_FOOTER_START_NUMBER =
-  mainExcel.items.length + TABLE_BODY_START_NUMBER;
+  EXCEL_CONFIG.TABLE_BODY_START_NUMBER + mainExcel.items.length;
 
-const borderStyle = {
-  top: { style: "thin" },
-  left: { style: "thin" },
-  bottom: { style: "thin" },
-  right: { style: "thin" },
+const applyStyleToCell = (cell, style) => {
+  Object.assign(cell, style);
 };
-const borderStyleBold = {
-  top: { style: "thick" },
-  left: { style: "thick" },
-  bottom: { style: "thick" },
-  right: { style: "thick" },
+
+const createHeaderCell = (worksheet, cellAddress, value) => {
+  const cell = worksheet.getCell(cellAddress);
+  cell.value = value;
+  cell.font = { bold: true };
+  return cell;
+};
+
+const createTableHeaderCell = (cell, value) => {
+  applyStyleToCell(cell, EXCEL_CONFIG.tableHeaderStyle);
+  cell.value = value;
 };
 
 const headerExcel = (worksheet) => {
-  worksheet.columns = [
-    { width: 20 },
-    { autoWidth: true },
-    { width: 20 },
-    { autoWidth: true },
-    { autoWidth: true },
-    { autoWidth: true },
-    { autoWidth: true },
-    { width: 20 },
-    { autoWidth: true },
-    { autoWidth: true },
-    { autoWidth: true },
-    { width: 5 },
-    { width: 15 },
-  ];
+  worksheet.columns = EXCEL_CONFIG.columns;
 
-  worksheet.getCell("A2").value =
-    "Name: " + employee.name + " " + employee.lastname;
-  worksheet.getCell("A2").font = { bold: true };
+  createHeaderCell(
+    worksheet,
+    "A2",
+    `Name: ${employee.name} ${employee.lastname}`
+  );
+  createHeaderCell(worksheet, "H2", `Invoice: ${mainExcel.invoiceNumber}`);
+  createHeaderCell(worksheet, "A3", `Invoice: ABN: ${employee.abn}`);
+  createHeaderCell(
+    worksheet,
+    "H3",
+    `Date: ${mainExcel.startDate} to ${mainExcel.endDate}`
+  );
+  createHeaderCell(worksheet, "F5", "Tax Invoice");
 
-  worksheet.getCell("H2").value = "Invioce: " + mainExcel.invioceNumer;
-  worksheet.getCell("H2").font = { bold: true };
-
-  worksheet.getCell("A3").value = "Invioce: " + "ABN: " + employee.abn;
-  worksheet.getCell("A3").font = { bold: true };
-
-  worksheet.getCell("H3").value =
-    "Date: " + mainExcel.startDate + " to " + mainExcel.endDate;
-  worksheet.getCell("H3").font = { bold: true };
-
-  worksheet.getCell("F5").value = "Tax Invioce";
-  worksheet.getCell("F5").font = { bold: true };
-
-  worksheet.getCell("A7").value = company.name;
-  worksheet.getCell("A7").font = { bold: true };
-
-  worksheet.getCell("A8").value = company.address;
-  worksheet.getCell("A8").font = { bold: true };
-
-  worksheet.getCell("A9").value = company.city;
-  worksheet.getCell("A9").font = { bold: true };
-
-  worksheet.getCell("A10").value = company.stateA;
-  worksheet.getCell("A10").font = { bold: true };
+  ["A7", "A8", "A9", "A10"].forEach((cellAddress, index) => {
+    createHeaderCell(
+      worksheet,
+      cellAddress,
+      [company.name, company.address, company.city, company.stateA][index]
+    );
+  });
 
   return worksheet;
 };
 
 const headerTableExcel = (worksheet) => {
-  worksheet.getCell("A" + TABLE_HEADER_START_NUMBER).value = "Unilodge";
-  worksheet.getCell("G" + TABLE_HEADER_START_NUMBER).value = company.address;
+  const { TABLE_HEADER_START_NUMBER, TABLE_HEADER_COLUMNS_START_NUMBER } =
+    EXCEL_CONFIG;
+
+  worksheet.getCell(`A${TABLE_HEADER_START_NUMBER}`).value = "Unilodge";
+  worksheet.getCell(`G${TABLE_HEADER_START_NUMBER}`).value = company.address;
 
   worksheet.mergeCells(
-    "A" + TABLE_HEADER_START_NUMBER + ":F" + TABLE_HEADER_START_NUMBER
+    `A${TABLE_HEADER_START_NUMBER}:F${TABLE_HEADER_START_NUMBER}`
   );
   worksheet.mergeCells(
-    "G" + TABLE_HEADER_START_NUMBER + ":M" + TABLE_HEADER_START_NUMBER
+    `G${TABLE_HEADER_START_NUMBER}:M${TABLE_HEADER_START_NUMBER}`
   );
-  // Define el rango de celdas desde A12 hasta M13
-  const startRow = TABLE_HEADER_START_NUMBER;
-  const endRow = TABLE_HEADER_COLUMNS_START_NUMBER;
-  const startCol = 1; // A
-  const endCol = 13; // M
 
-  // Itera sobre cada celda en el rango y aplica el estilo
-  for (let row = startRow; row <= endRow; row++) {
-    for (let col = startCol; col <= endCol; col++) {
-      const cell = worksheet.getCell(row, col);
-      cell.fill = {
-        type: "pattern",
-        pattern: "solid",
-        fgColor: { argb: "000000" }, // Negro
-      };
-      cell.font = {
-        name: "Arial",
-        color: { argb: "FFFFFF" }, // Blanco
-        bold: true,
-      };
-      cell.alignment = {
-        vertical: "middle",
-        horizontal: "center",
-      };
+  for (
+    let row = TABLE_HEADER_START_NUMBER;
+    row <= TABLE_HEADER_COLUMNS_START_NUMBER;
+    row++
+  ) {
+    for (let col = 1; col <= 13; col++) {
+      createTableHeaderCell(worksheet.getCell(row, col));
     }
   }
 
-  worksheet.getCell("A" + TABLE_HEADER_COLUMNS_START_NUMBER).value = "DATE";
-  worksheet.getCell("C" + TABLE_HEADER_COLUMNS_START_NUMBER).value =
-    "ROOM NUMBER AND TYPE";
-  worksheet.getCell("H" + TABLE_HEADER_COLUMNS_START_NUMBER).value =
-    "DESCRIPTION";
-  worksheet.getCell("L" + TABLE_HEADER_COLUMNS_START_NUMBER).value = "TIME";
-  worksheet.getCell("M" + TABLE_HEADER_COLUMNS_START_NUMBER).value = "AMOUNT";
+  const headerValues = [
+    "DATE",
+    "ROOM NUMBER AND TYPE",
+    "DESCRIPTION",
+    "TIME",
+    "AMOUNT",
+  ];
+  const headerCells = ["A", "C", "H", "L", "M"].map((col) =>
+    worksheet.getCell(`${col}${TABLE_HEADER_COLUMNS_START_NUMBER}`)
+  );
+  headerCells.forEach((cell, index) => (cell.value = headerValues[index]));
 
   worksheet.mergeCells(
-    "A" +
-      TABLE_HEADER_COLUMNS_START_NUMBER +
-      ":B" +
-      TABLE_HEADER_COLUMNS_START_NUMBER
+    `A${TABLE_HEADER_COLUMNS_START_NUMBER}:B${TABLE_HEADER_COLUMNS_START_NUMBER}`
   );
   worksheet.mergeCells(
-    "C" +
-      TABLE_HEADER_COLUMNS_START_NUMBER +
-      ":G" +
-      TABLE_HEADER_COLUMNS_START_NUMBER
+    `C${TABLE_HEADER_COLUMNS_START_NUMBER}:G${TABLE_HEADER_COLUMNS_START_NUMBER}`
   );
   worksheet.mergeCells(
-    "H" +
-      TABLE_HEADER_COLUMNS_START_NUMBER +
-      ":K" +
-      TABLE_HEADER_COLUMNS_START_NUMBER
+    `H${TABLE_HEADER_COLUMNS_START_NUMBER}:K${TABLE_HEADER_COLUMNS_START_NUMBER}`
   );
 
   return worksheet;
 };
+
 const bodyTableExcel = (worksheet) => {
-  for (let i = 0; i < mainExcel.items.length; i++) {
-    const rowNUmber = i + TABLE_BODY_START_NUMBER;
-    const row = worksheet.getRow(rowNUmber);
-    // Date
-    row.getCell("A").value = mainExcel.items[i].date;
-    row.getCell("A").alignment = {
-      vertical: "middle",
-      horizontal: "center",
-    };
-    row.getCell("A").dataType = ExcelJS.ValueType.Date;
-    row.getCell("A").numFmt = "dd/mm/yyyy";
-    worksheet.mergeCells(rowNUmber, 1, rowNUmber, 2);
+  const { TABLE_BODY_START_NUMBER } = EXCEL_CONFIG;
 
-    // room type
-    row.getCell("C").value = mainExcel.items[i].room;
-    row.getCell("C").alignment = {
-      vertical: "middle",
-      horizontal: "center",
-    };
-    worksheet.mergeCells(rowNUmber, 3, rowNUmber, 7);
+  mainExcel.items.forEach((item, i) => {
+    const rowNumber = i + TABLE_BODY_START_NUMBER;
+    const row = worksheet.getRow(rowNumber);
 
-    // Description
-    row.getCell("H").value = mainExcel.items[i].description;
-    row.getCell("H").alignment = {
-      vertical: "middle",
-      horizontal: "center",
-    };
-    worksheet.mergeCells(rowNUmber, 8, rowNUmber, 11);
+    applyStyleToCell(row.getCell("A"), EXCEL_CONFIG.dateCellStyle);
+    row.getCell("A").value = item.date;
+    worksheet.mergeCells(rowNumber, 1, rowNumber, 2);
 
-    // Time
+    applyStyleToCell(row.getCell("C"), EXCEL_CONFIG.centerAlignStyle);
+    row.getCell("C").value = item.room;
+    worksheet.mergeCells(rowNumber, 3, rowNumber, 7);
+
+    applyStyleToCell(row.getCell("H"), EXCEL_CONFIG.centerAlignStyle);
+    row.getCell("H").value = item.description;
+    worksheet.mergeCells(rowNumber, 8, rowNumber, 11);
+
+    applyStyleToCell(row.getCell("L"), EXCEL_CONFIG.centerAlignStyle);
     row.getCell("L").value = "";
-    //row.getCell("L").value = mainExcel.items[i].time;
-    row.getCell("L").alignment = {
-      vertical: "middle",
-      horizontal: "center",
-    };
 
-    // Amount
-    row.getCell("M").value = mainExcel.items[i].amount;
-    row.getCell("M").alignment = {
-      vertical: "middle",
-      horizontal: "center",
-    };
+    applyStyleToCell(row.getCell("M"), EXCEL_CONFIG.amountCellStyle);
+    row.getCell("M").value = item.amount;
+  });
 
-    row.getCell("M").numFmt =
-      '_($* #,##0.00_);_($* (#,##0.00);_($* "-"??_);_(@_)';
-  }
-
-  // Aplicar bordes a todas las celdas desde A12 hasta M21
   for (
-    let row = TABLE_HEADER_START_NUMBER;
+    let row = TABLE_BODY_START_NUMBER;
     row <= TABLE_FOOTER_START_NUMBER - 1;
     row++
   ) {
     for (let col = 1; col <= 13; col++) {
-      // A = 1, M = 13
-      const cell = worksheet.getCell(row, col);
-      cell.border = borderStyle;
+      worksheet.getCell(row, col).border = BORDER_STYLES.thin;
     }
   }
+
   return worksheet;
 };
 
 const footerTableExcel = (worksheet) => {
+  console.log("footerTableExcel", TABLE_FOOTER_START_NUMBER);
   const row = worksheet.getRow(TABLE_FOOTER_START_NUMBER);
-  row.getCell("L").value = "Total";
-  row.getCell("L").font = { bold: true };
-  row.getCell("L").border = borderStyleBold;
 
-  row.getCell("M").value = mainExcel.totalAmount;
-  row.getCell("M").font = { bold: true };
-  row.getCell("M").numFmt =
-    '_($* #,##0.00_);_($* (#,##0.00);_($* "-"??_);_(@_)';
-  row.getCell("M").border = borderStyleBold;
-  return worksheet;
-};
-
-const tableExcel = (worksheet) => {
-  mainExcel.items.length;
-  return bodyTableExcel(headerTableExcel(worksheet));
-};
-
-const exportExcel = (workbook) => {
-  // Generar el archivo Excel y descargarlo
-  workbook.xlsx.writeBuffer().then((buffer) => {
-    const blob = new Blob([buffer], {
-      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    });
-    saveAs(
-      blob,
-      "Invioce " + employee.name + " " + employee.lastname + ".xlsx"
-    );
+  applyStyleToCell(row.getCell("L"), {
+    ...EXCEL_CONFIG.boldStyle,
+    ...BORDER_STYLES.thick,
   });
-  return workbook;
+  row.getCell("L").value = "Total";
+
+  applyStyleToCell(row.getCell("M"), {
+    ...EXCEL_CONFIG.boldStyle,
+    ...EXCEL_CONFIG.amountCellStyle,
+    ...BORDER_STYLES.thick,
+  });
+  row.getCell("M").value = mainExcel.totalAmount;
+
+  return worksheet;
 };
 
 const generateExcel = () => {
   const workbook = new ExcelJS.Workbook();
-  const worksheet = workbook.addWorksheet("Invioce " + mainExcel.invioceNumer);
-  footerTableExcel(tableExcel(headerExcel(worksheet)));
-  exportExcel(workbook);
+  const worksheet = workbook.addWorksheet(`Invoice ${mainExcel.invoiceNumber}`);
+
+  headerExcel(worksheet);
+  headerTableExcel(worksheet);
+  bodyTableExcel(worksheet);
+  footerTableExcel(worksheet);
+
+  workbook.xlsx.writeBuffer().then((buffer) => {
+    const blob = new Blob([buffer], { type: EXCEL_CONFIG.mimeType });
+    saveAs(blob, `Invoice ${employee.name} ${employee.lastname}.xlsx`);
+  });
 };
 </script>
